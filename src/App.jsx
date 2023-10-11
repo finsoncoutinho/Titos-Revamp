93185cf1a55133bd536cf8f0d4c922330818a3a7
“finsoncoutinho” <finsoncoutinho2125@gmail.com>
2023-10-11 12:41:50 +0530

    feat: Login & store token in localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import LoginScreen from './pages/LoginScreen'
+import HomeScreen from './pages/HomeScreen'
+
 const App = () => {
-  return <div>My App</div>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<HomeScreen />} />
+        <Route path='login' element={<LoginScreen />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
