a629d317f4f72e3bddda46b3aecbf81edbbbed7e
“finsoncoutinho” <finsoncoutinho2125@gmail.com>
2023-10-13 13:17:31 +0530

    setup routes using react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,43 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import LoginScreen from './pages/LoginScreen'
-import HomeScreen from './pages/HomeScreen'
+
+import LoginScreen from './features/Auth/LoginScreen'
+import EventsListScreen from './features/EventsList/EventsListScreen'
+import AppLayout from './ui/AppLayout'
+import BookingsHistoryScreen from './features/BookingHistory/BookingsHistoryScreen'
+import EventDetailsScreen from './features/EventDetails/EventDetailsScreen'
+import CartScreen from './features/cart/CartScreen'
+import OrderScreen from './features/order/OrderScreen'
+import ProtectedRoutes from './ui/ProtectedRoutes'
+import PageNotFoundScreen from './features/pageNotFound.jsx/pageNotFoundScreen'
+
+// We wraped <OrderScreen/> and <AppLayout/> with ProtectedRoutes inorder to prevent non-authorized users from accessing the application
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<HomeScreen />} />
         <Route path='login' element={<LoginScreen />} />
+        <Route
+          path='/booking/:BookingRef'
+          element={
+            <ProtectedRoutes>
+              <OrderScreen />
+            </ProtectedRoutes>
+          }
+        />
+        <Route
+          element={
+            <ProtectedRoutes>
+              <AppLayout />
+            </ProtectedRoutes>
+          }
+        >
+          <Route path='/' element={<EventsListScreen />} />
+          <Route path='/event/:eventRef' element={<EventDetailsScreen />} />
+          <Route path='/cart' element={<CartScreen />} />
+          <Route path='bookings' element={<BookingsHistoryScreen />} />
+        </Route>
+        <Route path='*' element={<PageNotFoundScreen />} />
       </Routes>
     </BrowserRouter>
   )
